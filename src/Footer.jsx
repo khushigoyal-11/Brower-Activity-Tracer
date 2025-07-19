d4b580e23ac8da43bd4aaa4904374fa016c33955
Khushi Goyal <2002kushigoyal@gmail.com>
2025-07-20 02:09:48 +0530

    Updated Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer className="text-center pt-1 px-2">
-            <p className="text-dark">Browser Activity Tracker - Developed by <span><a href="https://github.com/Manraj29" target="_blank" className="font-bold text-info">Manraj Singh Virdi</a> </span></p>
+            <p className="text-dark">Browser Activity Tracer </p>
         </footer>
     );
 }
